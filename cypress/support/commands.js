58981770db7cc39b29e0271ee95b0f5ba135b4d8
Ismail Messaoudi <ismail.messaoudi@mni.thm.de>
2025-12-01 18:15:42 +0100

    fix: resolve cypress eslint errors and add minimal commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,10 @@
+// cypress/support/commands.js
+Cypress.Commands.add('createTodo', (title) => {
+  cy.get('#task-title').type(title);
+  cy.get('#todo-form').submit();
+});
 
+Cypress.Commands.add('clearAllData', () => {
+  cy.clearLocalStorage();
+  cy.reload();
+});
